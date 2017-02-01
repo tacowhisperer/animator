5d03cbc54c5bb263585084b20d125dd8c2d94005
Andres Salgado <chato94@gmail.com>
2017-01-31 21:21:19 -0600

    Moved Chrome code to a separate file to speed up the debugging process

---
+++ test.js
@@ -0,0 +1,11 @@
+var e = document.body,
+	e2 = document.getElementById('lga').firstChild,
+	transF = {'background-color': ['rgb(66,133,244)', 45]},
+	transF2 = {'padding-top': ['0px', 45, 'trig']},
+	transB = {'background-color': ['white', 45]},
+	transB2 = {'padding-top': ['113px', 45, 'trig']},
+	transF3 = {'border': ['10px black', 45, 'trig']},
+	cssAnimator = new CSSAnimator();
+
+e.style.border = '1px white';
+cssAnimator.animate (e, transF3);
