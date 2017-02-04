3aa9e7515be615a54ceda17b2c7ac0f47bf30925
Andres Salgado <chato94@gmail.com>
2017-02-04 00:06:38 -0600

    Updated test code to find and squash enqueued animation abnormal behavior

--- test.js
+++ test.js
@@ -6,7 +6,9 @@ var e = document.body,
 	transB2 = {'padding-top': ['113px', 45, 'trig']},
 	transF3 = {'border': ['10px dotted black', 45, 'arctrig']},
 	transB3 = {'border': ['0px solid white', 45, '-exp-medium']},
+	transF4 = {'border-width': ['10px', 90, 'trig'], 'border-color': ['black', 10]},
+	transB4 = {'border-width': ['0px', 90, 'trig'], 'border-color': ['white', 10]}
 	cssAnimator = new CSSAnimator();
 
-e.style.border = '1px solid white';
-cssAnimator.animate (e, transF3);
+e.style.border = '0px solid white';
+cssAnimator.thenAnimate (e, transF4).thenAnimate (e, transB4);
