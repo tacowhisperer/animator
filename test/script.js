05938aea4cb3ed5e358e84eb6d2c7ac8a3642865
tacowhisperer <chato94@gmail.com>
2017-02-10 11:26:45 -0600

    Made changes

--- test/script.js
+++ test/script.js
@@ -1,25 +1,26 @@
 document.addEventListener ('DOMContentLoaded', function (loadedEvent) {
 	var e = document.getElementById ('rainbow'),
-		numFrames = 11,
+		numFrames = 120,
 		anim = new CSSAnimator (),
 		color = 0,
-		focusIn = false;
-		rainbow = ['red', 'green', 'blue'];
+		rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo'],
+		antiColor = Math.ceil ((rainbow.length - 1) / 2);
 
 
 	e.addEventListener ('mouseover', function () {
-		anim.animate (e, {'background-color': [rainbow[color % rainbow.length], numFrames, 'trig']});
+		anim.animate (e, {'background-color': [rainbow[color % rainbow.length], numFrames, 'trig'],
+						  'border': ['20px dotted ' + rainbow[antiColor % rainbow.length], numFrames, 'trig']});
 	});
 
 	e.addEventListener ('mouseout', function () {
-		anim.animate (e, {'background-color': ['white', numFrames, 'trig']});
+		anim.animate (e, {'background-color': ['white', numFrames, 'trig'],
+						  'border': ['0px solid rgba(255, 255, 255, 0)', numFrames, 'trig']});
 	});
 
 	e.addEventListener ('mousedown', function (ev) {
 		if (ev.which === 1) {
-			focusIn = !focusIn;
 			anim.animate (e, {'background-color': [rainbow[++color % rainbow.length], numFrames, 'trig'],
-							  'border': [focusIn? '10px dotted black' : '0px solid white', numFrames, 'spring']});
+							  'border': ['10px dotted ' + rainbow[++antiColor % rainbow.length], numFrames, 'trig']});
 		}
 	});
 });
